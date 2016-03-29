dc61822ff31f64cf71fb2dc70c9ec73d5f8cb630
SpringDL <DanL4@live.ca>
2016-03-29 00:31:10 -0400

    Lb_Particles > DataGrid

---
+++ Assets/Lb_Particles/DataGrid/BallPivot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallPivot : MonoBehaviour {
+
+	public float rotationSpeed = 500;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 0, rotationSpeed * -Time.deltaTime);
+	}
+}
---
+++ Assets/Lb_Particles/DataGrid/GridTile.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridTile : MonoBehaviour {
+
+	ParticleSystem emitter;
+	public bool emit = false;
+
+	ParticleSystem.Particle[] particles;
+	int particlesAlive;
+
+	// Use this for initialization
+	void Start () {
+		emitter = GetComponent<ParticleSystem>();
+		emitter.Clear();
+		emitter.Stop();
+
+		particles = new ParticleSystem.Particle[emitter.maxParticles];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		particlesAlive = emitter.GetParticles(particles);
+		//Debug.Log("particles out: " + particlesAlive);
+
+		if (emit && particlesAlive == 0){
+			emitter.Clear();
+			emitter.Emit(1);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D col){
+		if (col.name == "Ball"){
+			emit = (emit)? false : true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D col){
+		if (col.name == "Ball"){
+			emit = (emit)? false : true;
+		}
+	}
+}
