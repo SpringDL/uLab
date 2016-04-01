471fbdb6b733fe4175a8b24bb0b701f9dbb7427a
SpringDL <DanL4@live.ca>
2016-04-01 00:24:00 -0400

    Lb_Particles > HammerSmash WIP

---
+++ Assets/Lb_Particles/HammerSmash/Bypass.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bypass : MonoBehaviour {
+
+    public bool smashingTime = false;
+    bool smashing = false;
+
+    public GameObject impactGO;
+    ImpactParticleController impactSS;
+
+	// Use this for initialization
+	void Start () {
+        impactSS = impactGO.GetComponent<ImpactParticleController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (smashingTime) {
+            smashingTime = false;
+            StartCoroutine("SmashingTime");
+            
+        }
+	}
+
+    IEnumerator SmashingTime() {
+        if (!smashing) {
+            smashing = true;
+            yield return new WaitForSeconds(0.1f);
+            impactSS.smashingTime = true;
+            smashing = false;
+        }else {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Lb_Particles/HammerSmash/ImpactParticleController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImpactParticleController : MonoBehaviour {
+
+    public bool smashingTime = false;
+
+    ParticleSystem impactSystem;
+
+	// Use this for initialization
+	void Start () {
+        impactSystem = GetComponent<ParticleSystem>();
+        impactSystem.Stop();
+        StartCoroutine("Impact");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    IEnumerator Impact ()
+    {
+        while (enabled) {
+            if (!smashingTime) {
+                yield return null;
+            } else {
+                //print("Booyah!");
+                impactSystem.Emit(1);
+
+                smashingTime = false;
+                yield return null;
+            }
+        }
+    }
+}
